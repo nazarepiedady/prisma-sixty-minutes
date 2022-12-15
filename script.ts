5f4f4f9693628fd97ebcae3920e56a9f84ca7d52
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-15 15:31:39 +0100

    chore: write a little program to create a new user

--- script.ts
+++ script.ts
@@ -1,2 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({ data: { name: 'Kyle' } })
+  console.log(user)
+}
+
+main()
+  .catch(e => {
+    console.log(e.message)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
