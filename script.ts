9f80ec572ae4b91bf89ef0915632962df899e2ba
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-15 16:36:22 +0100

    chore: alter create function to return also user preference data

--- script.ts
+++ script.ts
@@ -2,12 +2,21 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
+  await prisma.user.deleteMany()
   const user = await prisma.user.create({
     data: {
       name: 'Kyle',
       email: 'kyle@test.com',
-      age: 27
+      age: 27,
+      UserPreference: {
+        create: {
+          emailUpdate: true
+        },
+      },
     },
+    include: {
+      UserPreference: true,
+    }
   })
 
   console.log(user)
